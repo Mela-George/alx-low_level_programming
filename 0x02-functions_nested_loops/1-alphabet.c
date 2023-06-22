dcb3d68f72663b282966b69bf844dcc84744073b
Mela-George <egesi.promise@gmail>
2023-06-21 17:26:21 -0700

    prints in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,9 +2,9 @@
 /**
  * main - print the alphabet in lowercase followed by a new line
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	int i;
 	for (i = 97; i <= 122; i++)

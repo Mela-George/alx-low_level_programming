8739f25670eb7ce9e9135c6dc5ac2a99a011259c
Mela-George <egesi.promise@gmail>
2023-06-21 18:24:12 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _isalpha - Return 1 if input is uppercase/lowercase letter.Return 0 otherwise
+ * _isalpha - Return 1 if input is uppercase/lowercase.Return 0 otherwise
  *
  * @c: The character is in ASCII code
  *
